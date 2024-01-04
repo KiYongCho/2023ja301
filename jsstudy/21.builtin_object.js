83dc0ba7b9c12f658459e4ff2b2be19bda7b6c22
realchoky <realchoky@naver.com>
2024-01-04 13:43:19 +0900

    123

--- jsstudy/21.builtin_object.js
+++ jsstudy/21.builtin_object.js
@@ -1,18 +1,18 @@
 /* 빌트인 객체 */
 
 // global or globalThis (in nodejs)
-console.log(globalThis); // global object (node의 전역객체)
-console.log(this);  // {}
-console.log(globalThis === this); // false
+//console.log(globalThis); // global object (node의 전역객체)
+//console.log(this);  // {}
+//console.log(globalThis === this); // false
 // console.log(module.exports === this); // node 전역스코프에서의 this
 
 // window (in browser)
-console.log(window);
-console.log(this);
-console.log(window === this);
+//console.log(window);
+//console.log(this);
+//console.log(window === this);
 
 let uri = 'http://google.com?name=홍길동';
-let encodedUri = encodeURI(uri); 
+let encodedUri = encodeURI(uri);
 console.log(encodedUri); // http://google.com?name=%ED%99%8D%EA%B8%B8%EB%8F%99
 let decodedUri = decodeURI(encodedUri);
 console.log(decodedUri); // http://google.com?name=홍길동
