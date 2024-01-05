78c8e13fccc4b462988e9179b1a73383d262fd8a
realchoky <realchoky@naver.com>
2024-01-05 17:36:41 +0900

    123

---
+++ jsstudy/jsonweek1.js
@@ -0,0 +1,15 @@
+/*
+    [실습과제 1/5]
+    https://jsonplaceholder.typicode.com/posts 게시물
+    https://jsonplaceholder.typicode.com/comments 댓글
+    https://jsonplaceholder.typicode.com/users 작성자
+    1. "데이터가져오기" 버튼을 클릭하면 게시물JSON을 가져와 테이블에 표시한다.
+       (번호, 작성자명, 제목, 댓글보기버튼)
+    2. "댓글보기" 버튼을 클릭하면 클릭된 게시물의 댓글데이터를 가져와 게시물 하단에 표시한다.
+*/
+
+$(function() {
+
+    // 코딩과 함께 즐주말!
+
+});
