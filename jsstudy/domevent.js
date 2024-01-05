680af87ee03e939956b333df91192a3b3cc122b1
realchoky <realchoky@gmail.com>
2024-01-06 00:09:24 +0900

    123

--- jsstudy/domevent.js
+++ jsstudy/domevent.js
@@ -29,29 +29,59 @@ window.onload = function () {
       let txt3Val1 = parseFloat(txt3s[0].value);
       let txt3Val2 = parseFloat(txt3s[1].value);
       switch(this.value) {
-        case '+':
-          result3.innerHTML = txt3Val1 + txt3Val2;
-          break;
-        case '-':
-          result3.innerHTML = txt3Val1 - txt3Val2;
-          break;
-        case '*':
-          result3.innerHTML = txt3Val1 * txt3Val2;
-          break;
-        case '/':
-          result3.innerHTML = txt3Val1 / txt3Val2;
-          break;
+        case '+': result3.innerHTML = txt3Val1 + txt3Val2; break;
+        case '-': result3.innerHTML = txt3Val1 - txt3Val2; break;
+        case '*': result3.innerHTML = txt3Val1 * txt3Val2; break;
+        case '/': result3.innerHTML = txt3Val1 / txt3Val2;
       }
     });
   }
 
   // 실습 4) li를 클릭하면 화면 배경색을 변경하고 result4에 색상명 출력
-
+  const clcolor = document.getElementsByClassName('clcolor');
+  for (eachEle of clcolor) {
+    eachEle.addEventListener('click', function() {
+      document.body.style.backgroundColor = this.getAttribute('value');
+    });
+  }
+  
   // 실습 5) 체크한 과일명들을 result5에 출력
+  const result5 = document.getElementById('result5');
+  const clcb = document.getElementsByClassName('clcb');
+  const resultArr = [];
+  for (eachEle of clcb) {
+    eachEle.addEventListener("change", function() {
+      const thisVal = this.value;
+      const thisValIdx = resultArr.indexOf(thisVal);
+      thisValIdx<0 ? resultArr.push(thisVal) : resultArr.splice(thisValIdx, 1);
+      result5.innerHTML = resultArr;
+    });
+  }
 
   // 실습 6) 선택한 숫자의 구구단을 result6에 출력
+  const result6 = document.getElementById('result6');
+  const sel = document.getElementById('sel');
+  sel.addEventListener('change', function() {
+    const selVal = this.value;
+    if (this.value != '--select--') {
+      for (resultStr='', i=2; i<10; i++) {
+        resultStr += `${selVal} * ${i} = ${selVal*i} `;
+      }      
+      result6.innerHTML = resultStr;
+    } else {
+      result6.innerHTML = '';
+    }
+  });
 
   // 실습 7) 버튼을 클릭하면 학생데이터를 테이블에 출력
   // 홍길동 100 100 100, 강감찬 90 90 90, 이순신 80, 80, 80
+  const btnscore = document.getElementById('btnscore');
+  const tds = document.querySelectorAll('tbody td');
+  btnscore.addEventListener('click', function() {
+    const students = ['홍길동',100,100,100,'강감찬',90,90,90,'홍길동',80,80,80];
+    tds.forEach(function(ele, idx) {
+      ele.innerHTML = students[idx];
+    });
+  });
 
 };
--- jsstudy/domevent_jquery.js
+++ jsstudy/domevent_jquery.js
@@ -20,28 +20,48 @@ $(function () {
     let txt3Val1 = parseFloat($(".txt3").eq(0).val());
     let txt3Val2 = parseFloat($(".txt3").eq(1).val());
     switch($(this).val()) {
-      case '+':
-        $("#result3").html(txt3Val1 + txt3Val2);
-        break;
-      case '-':
-        $("#result3").html(txt3Val1 - txt3Val2);
-        break;
-      case '*':
-        $("#result3").html(txt3Val1 * txt3Val2);
-        break;
-      case '/':
-        $("#result3").html(txt3Val1 / txt3Val2);
-        break;
+      case '+':        $("#result3").html(txt3Val1 + txt3Val2);        break;
+      case '-':         $("#result3").html(txt3Val1 - txt3Val2);         break;
+      case '*':         $("#result3").html(txt3Val1 * txt3Val2);        break;
+      case '/':         $("#result3").html(txt3Val1 / txt3Val2);         break;
     }
   });
 
   // 실습 4) li를 클릭하면 화면 배경색을 변경하고 result4에 색상명 출력
+  $("#bgcolorselect li").on("click", function() {
+    $("body").css("background-color", $(this).attr('value'));
+  });
 
   // 실습 5) 체크한 과일명들을 result5에 출력
+  const checkedArray = [];
+  $("input[name='cb']").on('change', function() {
+    const thisVal = $(this).val();
+    $(this).prop("checked")==true 
+      ? checkedArray.push(thisVal) 
+      : checkedArray.splice(checkedArray.indexOf(thisVal),1);
+    $("#result5").text(checkedArray);    
+  });
 
   // 실습 6) 선택한 숫자의 구구단을 result6에 출력
+  $("#sel").on("change", function() {
+    const selectedVal = $(this).val();
+    if (selectedVal != '--select--') {
+      for (danTxt='', let i=1; i<10; i++) {
+        danTxt += `${selectedVal} * ${i} = ${selectedVal*i} `;
+      }
+      $("#result6").text(danTxt);
+    } else {
+      $("#result6").text('');
+    }
+  });
 
   // 실습 7) 버튼을 클릭하면 학생데이터를 테이블에 출력
-  // 홍길동 100 100 100, 강감찬 90 90 90, 이순신 80, 80, 80  
+  // 홍길동 100 100 100, 강감찬 90 90 90, 이순신 80, 80, 80
+  $("#btnscore").on('click', function() {
+    const students = ['홍길동',100,100,100,'강감찬',90,90,90,'홍길동',80,80,80];
+    $('#tbl tbody tr td').each(function(idx) {
+      $(this).text(students[idx]);
+    });
+  });
 
 });
