0c0f613b335404cbc354c05464a51d41a2db7562
realchoky <realchoky@naver.com>
2024-01-08 16:13:42 +0900

    123

---
+++ jsstudy/jsonex2.js
@@ -0,0 +1,6 @@
+// 1/8 실습 (할일목록 정리하기)
+// https://jsonplaceholder.typicode.com/todos
+// 화면의 왼쪽에는 완료된 일의 목록을 표시(completed:'true')
+// 화면의 오른쪽에는 해야할 일의 목록을 표시 (completed: 'false')
+// 해야할 일의 각 줄에는 완료 버튼 표시
+// 완료 버튼 클릭시 왼쪽 완료된 일로 이동
