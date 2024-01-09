bdaaf2baa16e1c211647ff883d338daf90a3ef15
realchoky <realchoky@naver.com>
2024-01-09 16:04:28 +0900

    123

---
+++ jsstudy/jsonex3.js
@@ -0,0 +1,30 @@
+/* 실습 1/9 */
+
+// https://jsonplaceholder.typicode.com/users
+// users 데이터를 가져와 리스팅한다.
+// 상단에 검색옵션(아이디<id>, 이름<name>, 별칭<username>)으로 검색을
+// 구현한다. (문자열 검색시는 정규표현식 사용)
+
+{
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  }
