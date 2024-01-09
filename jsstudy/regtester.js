74bd8a334406ba416c3030982b24a07c9f4393ae
realchoky <realchoky@naver.com>
2024-01-09 14:46:45 +0900

    1231

--- jsstudy/regtester.js
+++ jsstudy/regtester.js
@@ -3,11 +3,29 @@ $(function() {
     $("#btn").on("click", function() {
         let flagStr = '';
         $("input:checked").toArray().forEach(inEle => {
-            flagStr += $(inEle).attr('value');    
+            const eleVal = $(inEle).attr('value');
+            if (eleVal!='test' && eleVal!='match') {
+                flagStr += $(inEle).attr('value'); 
+            }
         });
-        $("#result").text(
-            new RegExp($("#pattern").val(), flagStr)
-                .test($("#str").val()));
+        $("input:checked").toArray().forEach(inEle => {
+            const eleVal = $(inEle).attr('value');
+            if (eleVal=='test') printTest(flagStr);
+            if (eleVal=='match') printMatch(flagStr);
+        });
     });
-
 });
+
+function printTest(flagStr) {
+    $("#result").text(
+        new RegExp($("#pattern").val(), flagStr)
+            .test($("#str").val()));
+}
+
+function printMatch(flagStr) {
+    $("#result2").text(
+        $("#str").val().match(
+            new RegExp($("#pattern").val(), flagStr)
+        )
+    );
+}
