b6e828d86b83686d67cfc7c4d4274d3c79272871
realchoky <realchoky@naver.com>
2024-01-11 16:27:36 +0900

    123

---
+++ jsstudy/event1.js
@@ -0,0 +1,41 @@
+// attribute
+function clickBtn1() {
+    console.log("btn1 클릭!");
+};
+
+// property
+const btn2 = document.getElementById("btn2");
+btn2.onclick = function() {
+    console.log("btn2 클릭!");
+};
+
+// addEventListener
+const btn3 = document.getElementById("btn3");
+const func1 = function() {
+    console.log("btn3 클릭! 1번 핸들러 호출!");
+};
+btn3.addEventListener("click", func1);
+const func2 = function(e) {
+    console.log(e);
+    console.log(e.type);
+    console.log(e.target);
+    console.log(e.currentTarget);
+    console.log(e.eventPhase);
+    console.log(e.bubbles);
+    console.log(e.cancelable);
+    console.log(e.defaultPrevented);
+    console.log(e.isTrusted);
+    console.log(e.timeStamp);
+    console.log("btn3 클릭! 2번 핸들러 호출!");
+
+    console.log(e.target);
+    console.log(e.currentTarget);
+};
+btn3.addEventListener("click", func2);
+
+btn3.removeEventListener("click", func1);
+
+// jquery
+//$("#btn3").on("click", function() {
+//    console.log("btn3 클릭! 2번 핸들러 호출!");
+//});
